e36cdfedbcb7e3888e5a2898242de9545cc65ff5
lincay sidicina <lincaysidicina@MacBook-Pro-de-lincay.local>
2022-02-25 15:02:27 +0100

    accueil

---
+++ navbar.html.php
@@ -0,0 +1,24 @@
+
+
+<div class="container"> 
+<div class="dropdown">
+  
+
+<H3 class="text-center">Accueil</H3>
+<div class="row"> 
+<ul class="nav col-3 flex-column" id="top-menu">
+  <li class="nav-item">
+    <a class="nav-link active btn" href="#">Active</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link active btn" href="#">Link</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link text-reset btn" href="#">Link</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link text-reset btn " href="#">Disabled</a>
+  </li>
+</ul>
+
+
